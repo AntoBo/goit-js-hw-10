b30757c0d8d01d1c068c164f6cd2b29d3da4fb8c
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-22 14:29:25 +0200

    fetch

---
+++ src/fetchCountries.js
@@ -0,0 +1,18 @@
+export function fetchCountries(name) {
+  const searchParams = "name,capital,population,flags,languages";
+  const url = `https://restcountries.com/v3.1/name/${name.trim()}?fields=${searchParams}`;
+  console.log(url);
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("response is not OK: ", response.status);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("data is here: ", data);
+    })
+    .catch((error) => {
+      console.log("error is here: ", error);
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import './css/styles.css';
+import "./css/styles.css";
+// import * as IMP from "./fetchCountries.js";
+import { fetchCountries } from "./fetchCountries";
 
 const DEBOUNCE_DELAY = 300;
+
+fetchCountries(" ukraine");
