edce7a51c02a6b86fc517676256d43217ca4e119
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-23 11:37:48 +0200

    began

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,9 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { notifyOptions } from './notifyOptions';
+
 export function fetchCountries(name) {
   const searchParams = 'name,capital,population,flags,languages';
   const url = `https://restcountries.com/v3.1/name/${name.trim()}?fields=${searchParams}`;
-  console.log(url);
   fetch(url)
     .then(response => {
       if (!response.ok) {
@@ -13,6 +15,6 @@ export function fetchCountries(name) {
       console.log('data is here: ', data);
     })
     .catch(error => {
-      console.log('error is here: ', error);
+      Notify.failure('Oops, there is no country with that name', notifyOptions);
     });
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-import "./css/styles.css";
-import { fetchCountries } from "./fetchCountries";
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { notifyOptions } from './notifyOptions';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  card: document.querySelector('.country-info'),
+};
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-fetchCountries(" ukraine");
+function onInput(event) {
+  const searchValue = event.target.value;
+  if (searchValue.length > 0) {
+    fetchCountries(searchValue);
+  } else {
+    clearMarkup();
+  }
+}
+
+function clearMarkup() {
+  refs.card.innerHTML = '';
+  refs.list.innerHTML = '';
+  console.log('clearMarkup() done!');
+}
