5387157c8faef6063f37cb99f46f911b222af24d
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-23 10:30:49 +0200

    prettier

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,18 +1,18 @@
 export function fetchCountries(name) {
-  const searchParams = "name,capital,population,flags,languages";
+  const searchParams = 'name,capital,population,flags,languages';
   const url = `https://restcountries.com/v3.1/name/${name.trim()}?fields=${searchParams}`;
   console.log(url);
   fetch(url)
-    .then((response) => {
+    .then(response => {
       if (!response.ok) {
-        throw new Error("response is not OK: ", response.status);
+        throw new Error('response is not OK: ', response.status);
       }
       return response.json();
     })
-    .then((data) => {
-      console.log("data is here: ", data);
+    .then(data => {
+      console.log('data is here: ', data);
     })
-    .catch((error) => {
-      console.log("error is here: ", error);
+    .catch(error => {
+      console.log('error is here: ', error);
     });
 }
